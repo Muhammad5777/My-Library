2c10ccab24b297f8c1b77acb29d406af151cce6f
Muhammad5777 <ahmadajmal707@gmail.com>
2026-07-26 12:21:53 +0100

    Initial addition of `Mystical Journey`

---
+++ books/Mystical Journey/Create Mystical Journey.epub.ps1
@@ -0,0 +1,1 @@
+& "..\..\scripts\Create EPUB.ps1" -SourceFolder "." -OutputEpub ".\Mystical Journey.epub"
---
+++ books/Mystical Journey/prettier script.bat
@@ -0,0 +1,1 @@
+npx prettier . --print-width 99999 --write --html-whitespace-sensitivity css
